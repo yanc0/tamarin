8200c8e6a710fbf6d7a3375a9da8075f72ddaa11
Yann Coleu <y@nn-col.eu>
2018-05-06 13:49:33 +0200

    move all tree related function in lib dir

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/yanc0/tamarin/lib"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"reflect"
 	"sort"
 )
 
@@ -65,152 +65,17 @@ func main() {
 		log.Fatalf("error: %v", err)
 	}
 
-	t0 := mapToTree(y0)
-	t1 := mapToTree(y1)
-	//printTree(t0, "-")
-	//printTree(t1, "-")
+	t0 := lib.MapToTree(y0)
+	t1 := lib.MapToTree(y1)
 
-	fmt.Println("***************")
-	mergeTrees(t0, t1)
-	//printTree(t0, "-")
+	t0.MergeTree(t1)
 
-	fmt.Println("***************")
-	y, _ := yaml.Marshal(t0.ToMap())
+	y, err := yaml.Marshal(t0.ToMap())
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
 	fmt.Println(string(y))
 
-}
-
-func mergeTrees(t1 *tree, t2 *tree) {
-	if t2.isLeaf() {
-		t1 = t2
-	} else {
-		for _, n := range t2.nodes {
-			if n.isLeaf() {
-				t1.Append(n)
-			} else {
-				if _, ok := t1.nodes[n.name]; ok {
-					mergeTrees(t1.nodes[n.name], n)
-				} else {
-					t1.Append(n)
-				}
-			}
-		}
-	}
-}
-
-func mapToTree(m map[interface{}]interface{}) *tree {
-	t := makeTree()
-	for key := range m {
-		var keyTree *tree
-		switch reflect.ValueOf(m[key]).Kind() {
-		case reflect.Map:
-			keyTree = mapToTree(m[key].(map[interface{}]interface{}))
-			keyTree.name = key
-
-		case reflect.Slice:
-			keyTree = &tree{
-				name: key,
-			}
-			if isSliceContainsMap(m[key].([]interface{})) {
-				for i, mp := range m[key].([]interface{}) {
-					tmpTree := mapToTree(mp.(map[interface{}]interface{}))
-					tmpTree.name = i
-					keyTree.Append(tmpTree)
-				}
-				keyTree.isSliceOfMap = true
-			} else {
-				keyTree.value = m[key]
-			}
-
-		default:
-			keyTree = &tree{
-				name:  key,
-				value: m[key],
-			}
-		}
-		t.Append(keyTree)
-	}
-	return t
-}
-
-func makeTree() *tree {
-	nodes := make(map[interface{}]*tree)
-	return &tree{
-		nodes: nodes,
-	}
-}
-
-type tree struct {
-	name         interface{}
-	value        interface{}
-	isSliceOfMap bool
-	nodes        map[interface{}]*tree
-}
-
-func (t *tree) isRoot() bool {
-	return t.name == nil
-}
-
-func (t *tree) ToMap() map[interface{}]interface{} {
-	m := make(map[interface{}]interface{})
-
-	for _, n := range t.nodes {
-		if n.isLeaf() {
-			m[n.name] = n.value
-		} else {
-			if ! n.isSliceOfMap {
-				m[n.name] = n.ToMap()
-			} else {
-				s := make([]interface{}, 0)
-				for i := range n.nodes {
-					s = append(s, n.nodes[i].ToMap())
-				}
-				m[n.name] = s
-			}
-		}
-	}
-	return m
-}
-
-func (t *tree) Append(add *tree) {
-	if t.nodes == nil {
-		t.nodes = make(map[interface{}]*tree)
-	}
-	t.nodes[add.name] = add
-}
-
-func (t *tree) Copy() *tree {
-	tree := makeTree()
-	tree.name = t.name
-	if t.isLeaf() {
-		tree.value = t.value
-	} else {
-		for _, n := range t.nodes {
-			tree.Append(n.Copy())
-		}
-	}
-	return tree
-}
-
-func (t *tree) isLeaf() bool {
-	return len(t.nodes) == 0
-}
-
-func isSliceContainsMap(m []interface{}) bool {
-	return len(m) > 0 && reflect.ValueOf(m[0]).Kind() == reflect.Map
-}
-
-func printTree(t *tree, prefix string) {
-	if len(t.nodes) == 0 {
-		fmt.Println(prefix, t.name, t.value)
-	} else {
-		if t.name != nil {
-			fmt.Println(prefix, t.name)
-		}
-		for _, st := range t.nodes {
-			printTree(st, prefix+"-")
-		}
-	}
 }
 
 type stringsByLen []string
